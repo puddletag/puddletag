84b7dcb2c513a4f22d87f453fd85c1c5651ef17c
cpuddle <devnull@localhost>
2010-02-03 19:34:39 +0000

    Added Filter Dialog (found in filterwin.py) and added the applyFilter method to TagModel. Works better than the old version (which hid rows instead of removing them). --forgot to add filterwin.py in previous commit.

---
+++ source/puddlestuff/mainwin/filterwin.py
@@ -0,0 +1,35 @@
+from PyQt4.QtGui import QWidget, QLabel, QComboBox, QLineEdit, QHBoxLayout
+from PyQt4.QtCore import SIGNAL
+from puddlestuff.puddleobjects import gettaglist
+
+class FilterView(QWidget):
+    def __init__(self, parent=None, status=None):
+        QWidget.__init__(self, parent)
+        self.emits = ['filter']
+        self.receives = []
+        hbox = QHBoxLayout()
+        hbox.addWidget(QLabel("Tag:"))
+        self._tags = QComboBox()
+        self._tags.addItems(['None', '__all'] + gettaglist())
+        self._tags.setEditable(True)
+        self._text = QLineEdit()
+        hbox.addWidget(self._tags, 0)
+        hbox.setMargin(0)
+        hbox.addWidget(self._text, 1)
+        self.setLayout(hbox)
+
+        self.connect(self._text, SIGNAL("textChanged(QString)"),
+                        self.valsChanged)
+        self.connect(self._tags, SIGNAL("editTextChanged(QString)"),
+                        self.valsChanged)
+
+    def valsChanged(self, *args):
+        tag = unicode(self._tags.currentText())
+        text = unicode(self._text.text())
+        if tag == 'None':
+            tag = None
+        else:
+            tag = [z.strip() for z in tag.split('|') if z.strip()]
+        self.emit(SIGNAL('filter'), tag, text)
+
+control = ("Filter", FilterView)
