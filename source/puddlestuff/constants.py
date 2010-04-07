92d1a5d86412bfcb99935f6337e03113402c4b20
cpuddle <devnull@localhost>
2010-04-07 11:23:02 +0000

    forgot constants

---
+++ source/puddlestuff/constants.py
@@ -0,0 +1,37 @@
+import os
+from PyQt4.QtCore import Qt
+from os.path import dirname, join
+#Paths
+HOMEDIR = os.getenv('HOME')
+SAVEDIR = join(HOMEDIR,'.puddletag')
+CONFIG = join(SAVEDIR, 'puddletag.conf')
+PLUGINDIR = join(SAVEDIR, 'plugins')
+PROGDIR = dirname(dirname(__file__))
+DATADIR = join(dirname(__file__), 'data')
+
+#Values used for controls in creating functions in actiondlg
+TEXT = 'text'
+COMBO = 'combo'
+CHECKBOX = 'check'
+TAGLIST = 'taglist'
+
+#Plugin contstants
+FORMATFUNCTIONS = 'FORMATFUNCTIONS'
+FUNCTIONS = 'FUNCTIONS'
+TAGSOURCE = 'TAGSOURCE'
+
+#Dock Positions
+LEFTDOCK = Qt.LeftDockWidgetArea
+RIGHTDOCK = Qt.RightDockWidgetArea
+BOTTOMDOCK = Qt.BottomDockWidgetArea
+TOPDOCK = Qt.TopDockWidgetArea
+
+#Tag constants
+PATH = u"__path"
+FILENAME = u"__filename"
+EXTENSION = '__ext'
+DIRPATH = '__dirpath'
+READONLY = ('__bitrate', '__frequency', "__length",
+            "__modified", "__size", "__created", "__library")
+FILETAGS = [PATH, FILENAME, EXTENSION, DIRPATH]
+INFOTAGS = FILETAGS + list(READONLY)
--- source/setup.py
+++ source/setup.py
@@ -2,18 +2,17 @@ from distutils.core import setup
 import sys
 
 setup(name='puddletag',
-      version='0.8.1',
+      version='0.8.5',
       author='concentricpuddle',
       author_email='concentricpuddle@gmail.com',
       url='http://puddletag.sourceforge.net',
       download_url='https://sourceforge.net/projects/puddletag/files/latest',
       description='A tag editor for Linux based on Mp3tag.',
-      long_description='None',
-      #package_dir={'puddletag': 'puddlestuff'},
       packages = ['puddlestuff', 'puddlestuff.mainwin', 'puddlestuff.duplicates',
-                    'puddlestuff.libraries', 'puddlestuff.audioinfo'],
+                    'puddlestuff.libraries', 'puddlestuff.audioinfo',
+                    'puddlestuff.tagsources', 'puddlestuff.data'],
       keywords='tagging ogg mp3 apev2 mp4',
-      license='GNU General Public License v3',
+      license='GNU General Public License v2',
       classifiers=['Development Status :: 2 - Unstable',
                    'Intended Audience :: Users',
                    'Natural Language :: English',
@@ -22,5 +21,7 @@ setup(name='puddletag',
                    'License :: OSI Approved :: GNU General Public License v2',
                    'Topic :: Tagging',
                   ],
-        scripts = ['puddletag', 'console']
+    scripts = ['puddletag'],
+    #package_dir={'puddlestuff.data': 'puddlestuff/data'},
+    #package_data={'puddlestuff.data': ['puddlestuff/data/shortcuts', 'puddlestuff/data/menus']}
      )
