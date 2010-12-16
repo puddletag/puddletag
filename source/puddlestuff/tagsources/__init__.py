81e7138b5530ea29c29f68f80aaabe6223834bdb
cpuddle <devnull@localhost>
2010-12-16 19:10:31 +0000

    Again with the paths!

--- source/puddlestuff/tagsources/__init__.py
+++ source/puddlestuff/tagsources/__init__.py
@@ -107,14 +107,7 @@ def urlopen(url, mask=True):
             raise RetrievalError('HTTPError 403: Forbidden')
         elif page.code == 404:
             raise RetrievalError("Page doesn't exist")
-        filename = '/home/keith/a.html'
-        if os.path.exists(filename):
-            filename = '/home/keith/a1.html'
-        f = open(filename, 'w')
-        x = page.read()
-        f.write(x)
-        f.close()
-        return x
+        return page.read()
     except urllib2.URLError, e:
         msg = u'%s (%s)' % (e.reason.strerror, e.reason.errno)
         raise RetrievalError(msg)
