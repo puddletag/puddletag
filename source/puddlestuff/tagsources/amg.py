1a817b7e4baeb8e942174732cf9b38f9d7c03e72
cpuddle <devnull@localhost>
2013-10-13 21:25:05 +0200

    Error for amg search.

--- source/puddlestuff/tagsources/amg.py
+++ source/puddlestuff/tagsources/amg.py
@@ -473,7 +473,7 @@ def retrieve_album(url, coverurl=None, id_field=ALBUM_ID):
     return info, tracks, cover
 
 def search(album):
-    search_url = create_search(replace(u'/', u' '))
+    search_url = create_search(album.replace(u'/', u' '))
     write_log(u'Search URL - %s' % search_url)
     return urlopen(iri_to_uri(search_url))
 
