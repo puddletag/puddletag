231993685bc486fd70c812e0e84de51d040f0589
cpuddle <devnull@localhost>
2014-11-09 00:46:53 +0200

    Updated logo and added news file for v1.0.4

--- source/create_release.sh
+++ source/create_release.sh
@@ -28,8 +28,8 @@ mv _build/html $BUILD_DIR/website
 
 echo "Uploading files to sourceforge"
 cd $BUILD_DIR
-rsync -acP -e ssh website $USER@frs.sourceforge.net:/home/project-web/puddletag/htdocs
-rsync -acP -e ssh *.tar.gz *.deb $USER@frs.sourceforge.net:/home/frs/project/p/pu/puddletag/ 
-rsync -acP -e ssh *.bz2 $USER@frs.sourceforge.net:/home/frs/project/p/pu/puddletag/docs
+# rsync -acP -e ssh website $USER@frs.sourceforge.net:/home/project-web/puddletag/htdocs
+# rsync -acP -e ssh *.tar.gz *.deb $USER@frs.sourceforge.net:/home/frs/project/p/pu/puddletag/ 
+# rsync -acP -e ssh *.bz2 $USER@frs.sourceforge.net:/home/frs/project/p/pu/puddletag/docs
 
 echo "Release uploaded"
