puddletag/puddletag

commit b909f2bfb5e4146eb07ea1cb016af03c30a32c0c

Fix tagmodel signal connections
The bug was introduced with commit 26eda69b. Calling disconnect() on an
QObject disconnects all slots on all signals, and in this case prevents
any updates the the button states when files are loaded or selected.
It is unfortunate that we can't check if that slot is connected, so we
just have to try and catch the exception in case it is not.

fixes #2

source/puddlestuff/tagmodel.py
@@ -1469,7 +1469,10 @@ class TagTable(QTableView):
             self.resizeColumnsToContents()
             self.model().modelReset.connect(self.resizeColumnsToContents)
         else:
-            self.disconnect()
+            try:
+                self.model().modelReset.connect(self.resizeColumnsToContents)
+            except TypeError:
+                logging.debug("Tried to disconnect un-connected Resize-Slot")
 
     def _getSelectedTags(self):
         columns = dict([(v,k) for k,v in self.model().columns.items()])

commit 9dc3c3ed88b275ae64cca311612ac573be1e68e6

Fix column resize
The bug was introduced with commit b909f2bf. You obviously need to
DISconnect the signal if the setting is false. The log message even
says so.

fixes #24

source/puddlestuff/tagmodel.py
@@ -1457,7 +1457,7 @@ class TagTable(QTableView):
             self.model().modelReset.connect(self.resizeColumnsToContents)
         else:
             try:
-                self.model().modelReset.connect(self.resizeColumnsToContents)
+                self.model().modelReset.disconnect(self.resizeColumnsToContents)
             except TypeError:
                 logging.debug("Tried to disconnect un-connected Resize-Slot")
 