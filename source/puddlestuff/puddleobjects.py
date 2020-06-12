puddletag/puddletag

commit 696d3961d4f3a9e5f8fd6c714cc4b92edc6ad271

[FIX] make drag and drop image to work
- Remove .decode() as it's already read as unicode
- Solve argument passing incompatibility, as the list is encapsulated in a tuple.

source/puddlestuff/puddleobjects.py
@@ -1974,6 +1974,8 @@ class PicWidget(QWidget):
         from .tagsources import RetrievalError, urlopen
         images = []
         
+        if isinstance(filenames, tuple):
+            filenames = filenames[0]
         for filename in filenames:
             image = QImage()
             if filename.startswith(":/"):

source/puddlestuff/tagsources/__init__.py
@@ -197,9 +197,9 @@ def urlopen(url, mask=True, code=False):
             raise RetrievalError(
                 translate("Tag Sources", "Page doesn't exist"))
         if code:
-            return page.read().decode(), page.code
+            return page.read(), page.code
         else:
-            return page.read().decode()
+            return page.read()
     except six.moves.urllib.error.URLError as e:
         try:
             msg = u'%s (%s)' % (e.reason.strerror, e.reason.errno)

commit a703d21dc134e529dd85b44d2c681f1990f157e2

Fix single image add
This bug was introduced with commit 696d3961. Since the arguments are
always packed with the * operator, `filenames` is always a tuple. The
real issue was the unpacking further up the stack.

source/puddlestuff/puddleobjects.py
@@ -1533,7 +1533,7 @@ class PicWidget(QWidget):
 
         self.label.setAlignment(Qt.AlignCenter)
         self.label.newImages.connect(
-            lambda *filenames: self.addImages(self.loadPics(*filenames)))
+            lambda filenames: self.addImages(self.loadPics(*filenames)))
 
         self._image_size = QLabel()
         self._image_size.setAlignment(Qt.AlignHCenter)
@@ -1949,13 +1949,14 @@ class PicWidget(QWidget):
         self.imageChanged.emit()
 
     def loadPics(self, *filenames):
-        """Loads pictures from the filenames"""
+        """Loads pictures from the filenames.
+
+        The filenames need to be passes as str arguments, one filename
+        per argument. Lists and tuples need to be unpacked by the caller."""
         #I really need to sort out these circular references.
         from .tagsources import RetrievalError, urlopen
         images = []
         
-        if isinstance(filenames, tuple):
-            filenames = filenames[0]
         for filename in filenames:
             image = QImage()
             if filename.startswith(":/"):