puddletag/puddletag

commit 3d0d8826a49379f74c358b908e0e753b492c1eeb

Fix QMessageBox
With Qt5, they obsoleted variants of the constructor, the static
factory methods and some buttons. Where possible I migrated to the
new simplified calls and fixed where needed.

source/puddlestuff/puddleobjects.py
@@ -41,8 +41,6 @@ from six import StringIO
 import itertools
 import logging
 
-MSGARGS = (QMessageBox.Warning, QMessageBox.Yes or QMessageBox.Default,
-    QMessageBox.No or QMessageBox.Escape, QMessageBox.YesAll)
 from functools import partial
 from .translations import translate
 
@@ -485,8 +483,7 @@ def get_languages(dirs=None):
     return ret
 
 def singleerror(parent, msg):
-    QMessageBox.warning(parent, 'Error', msg, QMessageBox.Ok,
-        QMessageBox.NoButton)
+    QMessageBox.warning(parent, 'Error', msg)
 
 def errormsg(parent, msg, maximum):
     """Shows a messagebox containing an error message indicating that
@@ -502,13 +499,16 @@ def errormsg(parent, msg, maximum):
         False if No.
         None if just yes."""
     if maximum > 1:
-        mb = QMessageBox(translate("Defaults", 'Error'),
+        mb = QMessageBox(QMessageBox.Warning, translate("Defaults", 'Error'),
             msg + translate("Defaults", "<br /> Do you want to continue?"),
-            *(MSGARGS + (parent, )))
+            QMessageBox.Yes or QMessageBox.No or QMessageBox.YesToAll,
+            parent)
+        mb.setDefaultButton(QMessageBox.Yes)
+        mb.setEscapeButton(QMessageBox.No)
         ret = mb.exec_()
         if ret == QMessageBox.No:
             return False
-        elif ret == QMessageBox.YesAll:
+        elif ret == QMessageBox.YesToAll:
             return True
     else:
         singleerror(parent, msg)

source/puddlestuff/helperwin.py
@@ -321,11 +321,9 @@ class ImportTextFile(QDialog):
 
             ret = QMessageBox.question(self,
                 translate('Text File -> Tag', "Error"),
-                translate('Text File -> Tag', errormsg.arg(filename)),
-                translate('Text File -> Tag', "&Yes"),
-                translate('Text File -> Tag', "&No"))
+                translate('Text File -> Tag', errormsg.arg(filename)))
 
-            if ret == 0:
+            if ret == QMessageBox.Yes:
                 return self.openFile()
             else:
                 return detail

source/puddlestuff/mainwin/funcs.py
@@ -149,13 +149,21 @@ def check_copy_data(data):
             "Do you want to go ahead?"))
 
         msgbox.setIcon(QMessageBox.Question)
-        msgbox.addButton(translate("Defaults", "&Yes"),
+        yesBtn = msgbox.addButton(translate("Defaults", "&Yes"),
             QMessageBox.YesRole)
-        msgbox.addButton(translate("Defaults", "No"),
+        noBtn = msgbox.addButton(translate("Defaults", "No"),
             QMessageBox.NoRole)
-        msgbox.addButton(translate("Messages", "Copy without images."),
+        noImgBtn = msgbox.addButton(translate("Messages", "Copy without images."),
             QMessageBox.ApplyRole)
-        return msgbox.exec_()
+
+        msgbox.exec_()
+
+        if msgbox.lockedButton() is yesBtn:
+          return 0
+        elif msgbox.lockedButton() is noBtn:
+          return 1
+        elif msgbox.lockedButton() is noImgBtn:
+          return 2
     else:
         return 0
 

source/puddlestuff/musiclib.py
@@ -213,9 +213,7 @@ class LibChooseDialog(QDialog):
                 'An error occured while loading the %1 library: <b>%2</b>')
             msg = msg.arg(self.currentlib['name']).arg(error_msg)
             
-            QMessageBox.critical(self, translate('Defaults', "Error"),
-                msg, QMessageBox.Ok, QMessageBox.NoButton,
-                QMessageBox.NoButton)
+            QMessageBox.critical(self, translate('Defaults', "Error"), msg)
         else:
             dialog = partial(LibraryDialog, library)
             self.adddock.emit(

source/puddlestuff/puddletag.py
@@ -545,12 +545,10 @@ class MainWin(QMainWindow):
         except (OSError, IOError) as e:
             QMessageBox.information(self._table,
                 translate("Defaults", 'Error'),
-                translate("Playlist", 'An error occured while reading <b>%1</b> (%2)').arg(filename).arg(e.strerror),
-                QMessageBox.Ok, QMessageBox.NoButton)
+                translate("Playlist", 'An error occured while reading <b>%1</b> (%2)').arg(filename).arg(e.strerror))
         except Exception as e:
             QMessageBox.information(self._table, translate("Defaults", 'Error'),
-                translate("Playlist", 'An error occured while reading <b>%1</b> (%2)').arg(filename).arg(six.text_type(e)),
-                QMessageBox.Ok)
+                translate("Playlist", 'An error occured while reading <b>%1</b> (%2)').arg(filename).arg(six.text_type(e)))
 
     def openDir(self, filename=None, append=False):
         """Opens a folder. If filename != None, then

source/puddlestuff/tagmodel.py
@@ -142,8 +142,7 @@ def has_previews(tags=None, parent=None, msg=None):
             break
 
     if previews and confirmations.should_show('preview_mode'):
-        ret = QMessageBox.question(parent, 'puddletag', msg,
-            QMessageBox.Yes, QMessageBox.No)
+        ret = QMessageBox.question(parent, 'puddletag', msg)
         if ret != QMessageBox.Yes:
             return True
     return False
@@ -1636,12 +1635,10 @@ class TagTable(QTableView):
     
         if delfiles and showmsg:
             result = QMessageBox.question(self, "puddletag",
-                translate("Table", "Are you sure you want to delete the selected files?"),
-                translate("Defaults", "&Yes"),
-                translate("Defaults", "&No"), "", 1, 1)
+                translate("Table", "Are you sure you want to delete the selected files?"))
         else:
-            result = 0
-        if result != 0:
+            result = QMessageBox.Yes
+        if result != QMessageBox.Yes:
             return
         selected = self.selectedTags
         selectedRows = self.selectedRows
@@ -2034,12 +2031,10 @@ class TagTable(QTableView):
                         translate("Table",
                                 "An error occurred while trying to play the selected files: <b>%1</b> "
                                 "<br />Does the music player you defined (<b>%2</b>)"
-                                " exist?").arg(detail.strerror).arg(" ".join(self.playcommand)),
-                            QMessageBox.Ok, QMessageBox.NoButton)
+                                " exist?").arg(detail.strerror).arg(" ".join(self.playcommand)))
                 else:
                     QMessageBox.critical(self, translate("Defaults", "Error"),
-                        translate("Table", "It wasn't possible to play the selected files, because the music player you defined (<b>%1</b>) does not exist.").arg(" ".join(self.playcommand)),
-                            QMessageBox.Ok, QMessageBox.NoButton)
+                        translate("Table", "It wasn't possible to play the selected files, because the music player you defined (<b>%1</b>) does not exist.").arg(" ".join(self.playcommand)))
     
     def previewMode(self, value):
         if not value:

commit 66636fcda5a9b32ee945bfa35039e6752ec195f4

Fix bulk error dialogue
Bug was introduced with 3d0d8826 and is the only one of its kind.
Obviously you need to use bitwise OR to build the button bitmask.

Fixes https://github.com/sandrotosi/puddletag/issues/62

source/puddlestuff/puddleobjects.py
@@ -515,7 +515,7 @@ def errormsg(parent, msg, maximum):
     if maximum > 1:
         mb = QMessageBox(QMessageBox.Warning, translate("Defaults", 'Error'),
                          msg + translate("Defaults", "<br /> Do you want to continue?"),
-                         QMessageBox.Yes or QMessageBox.No or QMessageBox.YesToAll,
+                         QMessageBox.Yes | QMessageBox.No | QMessageBox.YesToAll,
                          parent)
         mb.setDefaultButton(QMessageBox.Yes)
         mb.setEscapeButton(QMessageBox.No)