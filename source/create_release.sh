e0d35889d70507dfd0a8dc00cf7c8e5361deba0f
keithgg <concentricpuddle@gmail.com>
2020-08-29 15:26:52 +0200

    added docs folder for github pages so we don't have to use a different branch anymore

--- source/create_release.sh
+++ source/create_release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUILD_DIR=$(pwd)/releases/"release_"$(date +"%Y_%m_%d_%H%M%S")
+BUILD_DIR=$(pwd)/releases/"release_"$(date +"%Y_%m_%d")
 
 echo Created dir $BUILD_DIR
 mkdir -p $BUILD_DIR
@@ -15,7 +15,7 @@ python3 offlinezip.py $BUILD_DIR
 cd ../puddletag-docs
 python3 update_checksums.py checksums.txt $BUILD_DIR
 make html
-mv _build/html $BUILD_DIR/website
+mv _build/html ../docs/
+echo "docs.puddletag.net" > ../docs/CNAME
 
 echo "Release built"
-
--- source/puddlestuff/__init__.py
+++ source/puddlestuff/__init__.py
@@ -2,12 +2,12 @@
 import os
 from os.path import dirname
 
-version_string = '2.0.0'
-version = (2, 0, 0)
+version_string = "2.0.1"
+version = (2, 0, 1)
 changeset = None
 
 filedir = dirname(dirname(dirname(__file__)))
-hash_file = os.path.join(filedir, '.git/refs/heads/master')
+hash_file = os.path.join(filedir, ".git/refs/heads/master")
 if os.path.exists(hash_file):
     try:
         with open(hash_file) as fo:
