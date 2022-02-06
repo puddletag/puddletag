f68ee4b1c4ec7233edd0489c7aa62dcf71d3cca2
corubba <corubba@gmx.de>
2022-02-06 22:40:04 +0100

    Move translations
    There is a slight name clash since there already is a `translations.py`,
    but since this is just a folder and not a package it should be fine.

--- puddlestuff/resource.py
+++ puddlestuff/resource.py
@@ -8,4 +8,4 @@ from PyQt5.QtCore import QDir
 package_path = abspath(dirname(__file__))
 QDir.addSearchPath('data', join(package_path, 'data'))
 QDir.addSearchPath('icons', join(package_path, 'data'))
-QDir.addSearchPath('translations', join(package_path, '../translations'))
+QDir.addSearchPath('translations', join(package_path, 'translations'))
